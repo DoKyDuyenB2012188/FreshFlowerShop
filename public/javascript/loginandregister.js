14b2de81c654395a4b6a9a6fce927cfd609dfa04
dokyduyen <84983247+dokyduyen@users.noreply.github.com>
2022-12-08 14:06:29 +0700

    version2.8

--- public/javascript/loginandregister.js
+++ public/javascript/loginandregister.js
@@ -1,39 +1,102 @@
-function send() {
-    var email = document.getElementById('frm_Email').value;
-    var matKhau = document.getElementById('frm_MatKhau').value;
-    var ho = document.getElementById('frm_Ho').value;
-    var ten = document.getElementById('frm_Ten').value;
-    var diaChi = document.getElementById('frm_DiaChi').value;
-    var dienThoai = document.getElementById('frm_DienThoai').value;
+function register() {
+  var email = document.getElementById("frm_Email");
+  var matKhau = document.getElementById("frm_MatKhau");
+  var ho = document.getElementById("frm_Ho");
+  var ten = document.getElementById("frm_Ten");
+  var diaChi = document.getElementById("frm_DiaChi");
+  var dienThoai = document.getElementById("frm_DienThoai");
+  var filter = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
+  //Kiem tra thong tin bac buoc
+  if (
+    email.value == "" ||
+    matKhau.value == "" ||
+    ho.value == "" ||
+    ten.value == "" ||
+    diaChi.value == "" ||
+    dienThoai.value == ""
+  ) {
+    alert("Vui lòng điền hết * Thông tin");
+    return;
+  }
+  //kiem tra email
+  if (!filter.test(email.value)) {
+    alert("Hãy nhập đúng định dạng Email!");
+    email.focus();
+    return;
+  }
+  if (!(matKhau.value.length >= 8)) {
+    alert("Hãy nhập đúng mật khẩu phải từ 8 ký tự trở lên!");
+    matKhau.focus();
+    return;
+  }
+  //Kiem tra so dien thoai
+  if (isNaN(dienThoai.value)) {
+    alert("So điện thoại không hợp lệ! Vui lòng nhập lại");
+    return;
+  }
 
-    //Kiem tra thong tin bac buoc
-    if (email == "" || matKhau == "" || ho == "" || ten == "" || diaChi == "" || dienThoai == "") {
-        alert("Vui lòng điền hết * Thông tin");
-        return
-    }
-    //Kiem tra so dien thoai
-    if (isNaN(dienThoai)) {
-        alert("So điện thoại không hợp lệ! Vui lòng nhập lại");
-        return
-    }
-
-    //Kiem tra lai thong tin cua nguoi dang ky
-    var choice = confirm('Kiểm tra lại thông tin cá nhân\nEmail của bạn là: ' + email + '\nMật Khẩu: ' + matKhau + '\nHọ và Tên: ' + ho + ' ' + ten +'\nĐịa chỉ: ' + diaChi  + '\nĐiện thoại: ' + dienThoai );
-    if(choice == true){
-        alert("Bạn đã đăng ký thành công");
-    }
+  //Kiem tra lai thong tin cua nguoi dang ky
+  var choice = confirm(
+    "Kiểm tra lại thông tin cá nhân\nEmail của bạn là: " +
+      email.value +
+      "\nMật Khẩu: " +
+      matKhau.value +
+      "\nHọ và Tên: " +
+      ho.value +
+      " " +
+      ten.value +
+      "\nĐịa chỉ: " +
+      diaChi.value +
+      "\nĐiện thoại: " +
+      dienThoai.value
+  );
+  if (choice == true) {
+    window.localStorage.setItem(
+      "account",
+      JSON.stringify([
+        {
+          username: ten.value,
+          email: email.value,
+          password: matKhau.value,
+          login: false,
+        },
+      ])
+    );
+    alert("Bạn đã đăng ký thành công");
+  }
+  return flag;
 }
-function send(){
-    var email = document.getElementById('frm_Email').value;
-    var matKhau = document.getElementById('frm_MatKhau').value;
+function Login() {
+  alert("sss");
+  let frm = document.forms['login'];
+  let email = frm.frm_Email;
+  let matKhau = frm.frm_MatKhau;
+  var filter = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
 
-    //Kiem tra thong tin bat buoc
-    if(email == "" || matKhau == ""){
-        alert('Vui lòng nhập dữ liệu bắt buộc!');
-        return
-    }
+//   Kiem tra thong tin bat buoc
+  if (email.value == "" || matKhau.value == "") {
+    alert("Vui lòng nhập dữ liệu bắt buộc!");
+    return false;
+  }
 
-    var choice = confirm("Email: " + email + "\nMật khẩu: " +matKhau);
-    if(choice == true)
+  if (!filter.test(email.value)) {
+    alert("Hãy nhập đúng định dạng Email!");
+    email.focus();
+    return false;
+  }
+  let account = window.localStorage.getItem("account");
+  if(account){
+      account = JSON.parse(account)[0];
+    if (account.email == email.value && account.password == matKhau.value) {
+        account.login = true;
+        window.localStorage.setItem("account", JSON.stringify([account]));
         alert("Đăng nhập thành công!");
+        return true;
+      } else {
+        alert("Tài khoản hoặc mật khẩu sai!");
+      }
+  }else{
+    alert("Tài khoản hoặc mật khẩu sai!!");
+  }
+  return false;
 }
